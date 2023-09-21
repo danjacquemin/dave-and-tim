7930facaeaddeab1dd2814f54914c7328fe0e71e
Dan Jacquemin <jacquemin@gmail.com>
2023-09-20 23:15:08 -0400

    Added text and tshirt theme colors

--- src/components/swag-song.js
+++ src/components/swag-song.js
@@ -6,13 +6,46 @@ import Icons from '../icons'
 
 // -- -- --
 
+const lightTheme = {
+  text: '#000000',
+  background: '#ffffff',
+  sliderBg: '#000000',
+  tshirt: 'transparent',
+}
+
+const darkTheme = {
+  text: '#ffffff',
+  background: '#000000',
+  sliderBg: '#ffffff',
+  tshirt: 'transparent',
+}
+
+const themesMap = {
+  lightTheme,
+  darkTheme,
+}
+
+// -- -- --
+
+const Wrapper = styled.div`
+  ${tw`relative max-w-xl mx-auto text-lg`}
+`
+
 const Background = styled.div`
   ${tw`p-8`}
   font-family: 'Figtree', serif;
 `
-const Tshirt = styled.div`
-  ${tw``}
-`
+
+const Tshirt = styled.div.attrs((props) => ({
+  className: 'text-4xl font-semibold tracking-[.06em] mb-2',
+}))(
+  ({ theme }) => `
+  > svg {
+    fill: ${theme.colors.tshirt};
+  }
+  `
+)
+
 const Song = styled.div`
   ${tw`p-4`};
   position: absolute;
@@ -21,83 +54,78 @@ const Song = styled.div`
   width: 440px;
   scale: 70%;
 `
-const SongName = styled.div`
-  ${tw`text-4xl font-semibold tracking-[.06em] mb-2`}
-`
-const Band = styled.div`
-  ${tw`tracking-[.03em] pb-4`}
-`
-const Slider = styled.div`
-  ${tw`relative max-w-full h-16`}
-`
-const Track = styled.div`
-  ${tw`absolute top-0 w-full bg-black p-1 my-4 rounded opacity-25`}
-`
-const PlayedTrack = styled.div`
-  ${tw`absolute top-0 bg-black p-1 my-4 rounded`}
-  width: ${({ percent = 0 }) => percent}%;
-`
-const CurrentPosition = styled.div`
-  margin-left: -0.5em;
-  left: calc(${(props) => (props.percent === 0 ? `1em` : props.percent + `%`)} - 0.5em);
-  ${tw`absolute top-1 bg-black rounded-full p-4 w-[1em]`}
-`
-const MarkerTime = styled.div`
-  ${tw`absolute bottom-0 left-0`}
-`
-const EndTime = styled.div`
-  ${tw`absolute bottom-0 right-0`}
-`
-const Player = styled.div`
-  ${tw`p-4 flex flex-nowrap justify-between items-center`}
-`
-const SongLyrics = styled.div`
-  ${tw`py-4 pt-8 text-center leading-6 text-base tracking-[.02em]`}
-`
-
-const Wrapper = styled.div`
-  ${tw`relative max-w-xl mx-auto text-lg`}
-`
-
-// -- -- --
-
-const lightTheme = {
-  text: '#151515',
-  background: '#ffffff',
-}
-
-const darkTheme = {
-  text: '#ffffff',
-  background: '#151515',
-}
-
-const themesMap = {
-  lightTheme,
-  darkTheme,
-}
-
-// const Text = styled.p(
-//   ({ theme }) => `
-//     color: ${theme.colors.text};
-//     background-color:${theme.colors.background};
-//   `,
-//   `border: 1px dotted orange`
-// )
-
-// const Text = styled.p.attrs((props) => ({
-//   className: 'text-lg',
-// }))`
-//   ${tw`border`}
-// `
-
-const Text = styled.p.attrs((props) => ({
-  className: 'text-lg',
+const SongName = styled.div.attrs((props) => ({
+  className: 'text-4xl font-semibold tracking-[.06em] mb-2',
 }))(
   ({ theme }) => `
     color: ${theme.colors.text};
-    background-color:${theme.colors.background};
-  `,
-  `border: 1px dotted orange`
+  `
+)
+
+const Band = styled.div.attrs((props) => ({
+  className: 'tracking-[.03em] pb-4',
+}))(
+  ({ theme }) => `
+    color: ${theme.colors.text};
+  `
+)
+
+const Slider = styled.div.attrs((props) => ({
+  className: 'relative max-w-full h-16',
+}))(
+  ({ theme }) => `
+    color: ${theme.colors.text};
+  `
+)
+
+const Track = styled.div.attrs((props) => ({
+  className: 'absolute top-0 w-full p-1 my-4 rounded opacity-25',
+}))(
+  ({ theme }) => `
+    background-color: ${theme.colors.sliderBg};
+  `
+)
+
+const PlayedTrack = styled.div.attrs((props) => ({
+  className: `absolute top-0 p-1 my-4 rounded`,
+}))(
+  ({ theme }) => `
+    background-color: ${theme.colors.sliderBg};
+  `
+)
+
+const CurrentPosition = styled.div.attrs((props) => ({
+  className: `absolute top-1 rounded-full p-4 w-[1em]`,
+}))(
+  ({ theme }) => `
+    background-color: ${theme.colors.sliderBg};
+  `
+)
+
+const MarkerTime = styled.div`
+  ${tw`absolute bottom-0 left-0`}
+`
+
+const EndTime = styled.div`
+  ${tw`absolute bottom-0 right-0`}
+`
+
+const Player = styled.div.attrs((props) => ({
+  className: 'p-4 flex flex-nowrap justify-between items-center',
+}))(
+  ({ theme }) => `
+    > svg {
+      fill: ${theme.colors.sliderBg}
+    };
+  `
+)
+
+const Lyrics = styled.div.attrs((props) => ({
+  className: 'py-4 pt-8 text-center leading-6 text-base tracking-[.02em]',
+}))(
+  ({ theme }) => `
+    color: ${theme.colors.text};
+  `
 )
 
 // -- -- --
@@ -112,8 +140,16 @@ function timeToSeconds(timeAsString) {
 
 const SwagSong = () => {
   const [currentTheme, setCurrentTheme] = useState('lightTheme')
-
+  const [currentTShirt, setTShirt] = useState('transparent')
   const theme = { colors: themesMap[currentTheme] }
+
+  function handleShirtColor(value) {
+    theme.colors.tshirt = value
+    darkTheme.tshirt = value
+    lightTheme.tshirt = value
+    setTShirt(value)
+    console.log(theme)
+  }
 
   const songName = 'Crush'
   const bandName = 'Dave Matthews Band'
@@ -127,47 +163,59 @@ const SwagSong = () => {
   return (
     <Background>
       <ThemeProvider theme={theme}>
-        <Text>hello theme!</Text>
-        <select value={currentTheme} onChange={(e) => setCurrentTheme(e.target.value)}>
-          <option value="lightTheme">Light</option>
-          <option value="darkTheme">Dark</option>
-        </select>
+        <div>
+          <label for="text-color">Text</label>
+          <select id="text-color" value={currentTheme} onChange={(e) => setCurrentTheme(e.target.value)}>
+            <option value="darkTheme">Light</option>
+            <option value="lightTheme">Dark</option>
+          </select>
+        </div>
+        <div>
+          <label for="shirt-color">Shirt</label>
+          <select id="shirt-color" value={currentTShirt} onChange={(e) => handleShirtColor(e.target.value)}>
+            <option value="transparent">-</option>
+            <option value="#d3d3d3">Light Gray</option>
+            <option value="#650317">Maroon</option>
+            <option value="#0F3A9E">Royal Blue</option>
+            <option value="#F5B2C0">Baby Pink</option>
+            <option value="#0f172a">Very Dark Slate</option>
+          </select>
+        </div>
+        <Wrapper>
+          <Tshirt>
+            <Icons name="tshirt" color={currentTShirt} />
+          </Tshirt>
+          <Song className="light-icons">
+            <SongName>{songName}</SongName>
+            <Band>{bandName}</Band>
+            <Slider>
+              <PlayedTrack style={{ width: `${percentPlayed}%` }} />
+              <CurrentPosition
+                style={{
+                  marginLeft: `-0.5em`,
+                  left: `calc(${percentPlayed === 0 ? `1em` : percentPlayed + `%`} - 0.5em)`,
+                }}
+              />
+              <Track />
+              <MarkerTime>{songMarker}</MarkerTime>
+              <EndTime>{songLength}</EndTime>
+            </Slider>
+            <Player>
+              <Icons name="shuffle" additionalClasses="" />
+              <Icons name="backward-step" additionalClasses="icon-larger" />
+              <Icons name="circle-pause" additionalClasses="icon-largest" />
+              <Icons name="forward-step" additionalClasses="icon-larger" />
+              <Icons name="repeat-square" additionalClasses="icon-vertical-flip icon-green" />
+            </Player>
+            <Lyrics>
+              <div>It's crazy I'm thinking</div>
+              <div>Just knowing that the world is round</div>
+              <div>Here I'm dancing on the ground</div>
+              <div>Am I right side up or upside down</div>
+            </Lyrics>
+          </Song>
+        </Wrapper>
       </ThemeProvider>
-      <Wrapper>
-        <Tshirt>
-          <Icons name="tshirt" />
-        </Tshirt>
-        <Song className="light-icons">
-          <SongName>{songName}</SongName>
-          <Band>{bandName}</Band>
-          <Slider>
-            <PlayedTrack percent={percentPlayed} />
-            <CurrentPosition percent={percentPlayed} />
-            <Track />
-            <MarkerTime>
-              {/* songMarkerSeconds < songLengthSeconds ? songMarkerSeconds : songLengthSeconds */}
-              {songMarker}
-            </MarkerTime>
-            <EndTime>
-              {/* songLengthSeconds */}
-              {songLength}
-            </EndTime>
-          </Slider>
-          <Player>
-            <Icons name="shuffle" additionalClasses="" />
-            <Icons name="backward-step" additionalClasses="icon-larger" />
-            <Icons name="circle-pause" additionalClasses="icon-largest" />
-            <Icons name="forward-step" additionalClasses="icon-larger" />
-            <Icons name="repeat-square" additionalClasses="icon-vertical-flip icon-green" />
-          </Player>
-          <SongLyrics>
-            <div>It's crazy I'm thinking</div>
-            <div>Just knowing that the world is round</div>
-            <div>Here I'm dancing on the ground</div>
-            <div>Am I right side up or upside down</div>
-          </SongLyrics>
-        </Song>
-      </Wrapper>
     </Background>
   )
 }
